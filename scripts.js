5663bd8386fcd5111c542f75f1f4259d1835b779
gorlajagadeeshkumar <gorlajagadeeshkumar@gmail.com>
2023-10-29 20:11:28 -0500

    updated color palette

--- scripts.js
+++ scripts.js
@@ -2,11 +2,10 @@ document.querySelector('form').addEventListener('input', function() {
     let billTotal = parseFloat(document.getElementById('billTotal').value);
     let tip = parseInt(document.getElementById('tip').value);
 
-    if(isNaN(billTotal) || billTotal <= 0) {
+    if (isNaN(billTotal) || billTotal <= 0) {
         alert("Please enter a valid positive number for Bill Total!");
         return;
     }
-    
 
     document.getElementById('tipPercentage').value = tip + "%";
     let tipAmount = (billTotal * tip / 100).toFixed(2);
@@ -14,7 +13,6 @@ document.querySelector('form').addEventListener('input', function() {
 
     let totalBillWithTip = (billTotal + parseFloat(tipAmount)).toFixed(2);
     document.getElementById('totalBillWithTip').value = totalBillWithTip;
-
 
     if (tip <= 10) {
         document.getElementById('tipEmoji').innerHTML = '&#128542;'; // sad
@@ -25,15 +23,31 @@ document.querySelector('form').addEventListener('input', function() {
     }
 });
 
+function resetCalculator() {
+    document.getElementById('billTotal').value = '';
+    document.getElementById('tip').value = '10'; 
+    document.getElementById('tipPercentage').value = '10%'; 
+    document.getElementById('tipAmount').value = '';
+    document.getElementById('totalBillWithTip').value = '';
+    document.getElementById('tipEmoji').innerHTML = '&#128528;'; 
+}
+
 function printDetails() {
     let printContent = document.querySelector('.container').outerHTML;
     let printWindow = window.open('', '_blank');
-    
-    printWindow.document.write('<html><head><title>Print</title></head><body>');
+
+    printWindow.document.write('<html><head><title>Print</title>');
+    printWindow.document.write('<link rel="stylesheet" href="styles.css" type="text/css" />');
+    printWindow.document.write('</head><body>');
     printWindow.document.write(printContent);
     printWindow.document.write('</body></html>');
     printWindow.document.close();
     printWindow.print();
+
+    printWindow.onafterprint = function() {
+        printWindow.close();
+        window.focus();
+    };
+
+    resetCalculator();
 }
-
-
