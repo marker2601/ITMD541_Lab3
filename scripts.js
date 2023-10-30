173044efffb71478a4d70fd894e18b580913eb3e
gorlajagadeeshkumar <gorlajagadeeshkumar@gmail.com>
2023-10-29 23:01:54 -0500

    updated few changes for the view

--- scripts.js
+++ scripts.js
@@ -1,96 +1,74 @@
-document.querySelector('form').addEventListener('input', calculateTip);
+let billTotal = document.getElementById("billTotal");
+let people = document.getElementById("people");
+let serviceQuality = document.getElementById("serviceQuality");
+let tipSlider = document.getElementById("tip");
+let tipInput = document.getElementById("tipInput");
+let tipPercentage = document.getElementById("tipPercentage");
+let tipAmount = document.getElementById("tipAmount");
+let totalBillWithTip = document.getElementById("totalBillWithTip");
+let roundUp = document.getElementById("roundUp");
 
-//  to calculate the tip based on the bill amount, tip percentage, and number of people
 function calculateTip() {
-    var billAmt = parseFloat(document.getElementById("billamt").value);
-    var tipAmt = parseFloat(document.getElementById("tipamt").value);
-    var numOfPeople = parseInt(document.getElementById("peopleamt").value);
-
-    if (billAmt === "" || numOfPeople == 0) {
-        alert("Please enter values");
-        return;
+    let bill = parseFloat(billTotal.value);
+    let numberOfPeople = parseInt(people.value);
+    let tipPercent = parseInt(tipInput.value) / 100;
+    let serviceFactor = parseFloat(serviceQuality.value);
+    
+    let tipValue = bill * tipPercent * serviceFactor;
+    let totalTipAmount = tipValue / numberOfPeople;
+    
+    let totalBillValue = bill + tipValue;
+    
+    if (roundUp.checked) {
+        totalBillValue = Math.ceil(totalBillValue);
     }
 
-    var tipPercent = (tipAmt / billAmt) * 100;
-    document.getElementById("serviceQual").value = tipPercent.toFixed(2) + "%";
-
-    var total = (billAmt * tipPercent) / 100 / numOfPeople;
-    total = Math.round(total * 100) / 100;
-    total = total.toFixed(2);
-
-    document.getElementById("totalTip").style.display = "block";
-    document.getElementById("tip").innerHTML = total;
-
-    updateEmoji(tipPercent);
+    tipAmount.value = totalTipAmount.toFixed(2);
+    totalBillWithTip.value = totalBillValue.toFixed(2);
 }
 
-// to update the value of the tip percentage input when the slider changes
-function updateTipValue() {
-    let tipValue = document.getElementById('tip').value;
-    document.getElementById('tipInput').value = tipValue;
-    calculateTip();
-}
-
-//  to update the value of the slider when the tip percentage input changes
-function updateSliderValue() {
-    let tipInputValue = parseFloat(document.getElementById('tipInput').value);
-    document.getElementById('tip').value = tipInputValue;
-    calculateTip();
-}
-
-//  to adjust the tip percentage based on the selected service quality
 function adjustTipBasedOnService() {
-    let serviceMultiplier = parseFloat(document.getElementById('serviceQuality').value);
-    let newTip = parseFloat(document.getElementById('tip').value) * serviceMultiplier;
-    newTip = Math.min(newTip, 100);
-    document.getElementById('tip').value = newTip;
-    document.getElementById('tipInput').value = newTip;
+    let baseTip = parseFloat(tipSlider.value) / 100;
+    let serviceFactor = parseFloat(serviceQuality.value);
+    let adjustedTip = baseTip * serviceFactor;
+    
+    tipInput.value = (adjustedTip * 100).toFixed(0);
     calculateTip();
 }
 
-//  to update the emoji based on the tip percentage
-function updateEmoji(tip) {
-    let emojiElement = document.getElementById('tipEmoji');
-    if (tip <= 10) {
-        emojiElement.innerHTML = '&#128542;';
-    } else if (tip > 10 && tip <= 20) {
-        emojiElement.innerHTML = '&#128528;';
-    } else {
-        emojiElement.innerHTML = '&#128578;';
-    }
+function updateTipValue() {
+    let tipVal = tipSlider.value;
+    tipInput.value = tipVal;
+    calculateTip();
 }
 
+function updateSliderValue() {
+    let inputVal = tipInput.value;
+    if (inputVal > 100) inputVal = 100;
+    if (inputVal < 0) inputVal = 0;
+    
+    tipSlider.value = inputVal;
+    calculateTip();
+}
 
-//  to print the filled details
 function printDetails() {
-    let billAmt = document.getElementById("billamt").value;
-    let tipAmt = document.getElementById("tipamt").value;
-    let numOfPeople = document.getElementById("peopleamt").value;
-    let totalTipElem = document.getElementById("totalTip");
-
-
-    if (billAmt !== "" && numOfPeople !== 0 && totalTipElem.style.display !== "none") {
-        let printContent = `
-            <div class="print-container">
-                <h2>Tip Details</h2>
-                <p>Bill Amount: ${billAmt}</p>
-                <p>Tip Amount: ${tipAmt}</p>
-                <p>Number of People: ${numOfPeople}</p>
-                <p>Total Tip per Person: ${totalTipElem.innerHTML}</p>
-            </div>
-        `;
-
-        let printWindow = window.open('', '_blank');
-        printWindow.document.write('<html><head><title>Print</title></head><body>');
-        printWindow.document.write(printContent);
-        printWindow.document.write('</body></html>');
-        printWindow.document.close();
-        printWindow.print();
-    } else {
-        alert("Please fill in the details before printing.");
-    }
+    let details = "Bill Total: $" + billTotal.value + "\n";
+    details += "Number of People: " + people.value + "\n";
+    details += "Service Quality: " + serviceQuality.options[serviceQuality.selectedIndex].text + "\n";
+    details += "Tip Percentage: " + tipInput.value + "%\n";
+    details += "Tip Amount: $" + tipAmount.value + "\n";
+    details += "Total Bill with Tip: $" + totalBillWithTip.value + "\n";
+    
+    alert(details);
 }
 
-
-
-document.getElementById("totalTip").style.display = "none";
+document.querySelector("input[type='reset']").addEventListener("click", function() {
+    billTotal.value = "";
+    people.value = 1;
+    serviceQuality.value = 1;
+    tipSlider.value = 15;
+    tipInput.value = 15;
+    tipAmount.value = "";
+    totalBillWithTip.value = "";
+    roundUp.checked = false;
+});
