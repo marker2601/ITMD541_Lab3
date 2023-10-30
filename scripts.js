e3b864604b4d425717bafec2a9d2d104e327c564
gorlajagadeeshkumar <gorlajagadeeshkumar@gmail.com>
2023-10-29 19:52:37 -0500

    updated scripts to consider the values more than 0 for the billing

--- scripts.js
+++ scripts.js
@@ -2,10 +2,11 @@ document.querySelector('form').addEventListener('input', function() {
     let billTotal = parseFloat(document.getElementById('billTotal').value);
     let tip = parseInt(document.getElementById('tip').value);
 
-    if(isNaN(billTotal)) {
-        alert("Please enter a valid number for Bill Total!");
+    if(isNaN(billTotal) || billTotal <= 0) {
+        alert("Please enter a valid positive number for Bill Total!");
         return;
     }
+    
 
     document.getElementById('tipPercentage').value = tip + "%";
     let tipAmount = (billTotal * tip / 100).toFixed(2);
