c8cb4337fd47b36b09b8e24b2e51a874e3da1a2c
gorlajagadeeshkumar <gorlajagadeeshkumar@gmail.com>
2023-10-29 22:12:54 -0500

    updated people and split information for the JS file

--- scripts.js
+++ scripts.js
@@ -1,53 +1,67 @@
-document.querySelector('form').addEventListener('input', function() {
+document.querySelector('form').addEventListener('input', calculateTip);
+
+function calculateTip() {
     let billTotal = parseFloat(document.getElementById('billTotal').value);
     let tip = parseInt(document.getElementById('tip').value);
+    let people = parseInt(document.getElementById('people').value);
 
-    if (isNaN(billTotal) || billTotal <= 0) {
+    if(isNaN(billTotal) || billTotal <= 0) {
         alert("Please enter a valid positive number for Bill Total!");
         return;
     }
 
-    document.getElementById('tipPercentage').value = tip + "%";
     let tipAmount = (billTotal * tip / 100).toFixed(2);
-    document.getElementById('tipAmount').value = tipAmount;
-
     let totalBillWithTip = (billTotal + parseFloat(tipAmount)).toFixed(2);
-    document.getElementById('totalBillWithTip').value = totalBillWithTip;
 
-    if (tip <= 10) {
-        document.getElementById('tipEmoji').innerHTML = '&#128542;'; // sad
-    } else if (tip > 10 && tip <= 20) {
-        document.getElementById('tipEmoji').innerHTML = '&#128528;'; // neutral
-    } else {
-        document.getElementById('tipEmoji').innerHTML = '&#128578;'; // smiling
+    if(document.getElementById('roundUp').checked) {
+        totalBillWithTip = Math.ceil(totalBillWithTip);
+        tipAmount = (totalBillWithTip - billTotal).toFixed(2);
     }
-});
 
-function resetCalculator() {
-    document.getElementById('billTotal').value = '';
-    document.getElementById('tip').value = '10'; 
-    document.getElementById('tipPercentage').value = '10%'; 
-    document.getElementById('tipAmount').value = '';
-    document.getElementById('totalBillWithTip').value = '';
-    document.getElementById('tipEmoji').innerHTML = '&#128528;'; 
+    document.getElementById('tipPercentage').value = tip + "%";
+    document.getElementById('tipAmount').value = tipAmount;
+    document.getElementById('totalBillWithTip').value = totalBillWithTip / people;
+
+    updateEmoji(tip);
+}
+
+function updateTipValue() {
+    let tipValue = document.getElementById('tip').value;
+    document.getElementById('tipInput').value = tipValue;
+    calculateTip();
+}
+
+function updateSliderValue() {
+    let tipInputValue = document.getElementById('tipInput').value;
+    document.getElementById('tip').value = tipInputValue;
+    calculateTip();
+}
+
+function adjustTipBasedOnService() {
+    let serviceMultiplier = parseFloat(document.getElementById('serviceQuality').value);
+    let newTip = parseInt(document.getElementById('tip').value) * serviceMultiplier;
+    newTip = Math.min(newTip, 100);  // Ensure tip doesn't exceed 100%
+    document.getElementById('tip').value = newTip;
+    document.getElementById('tipInput').value = newTip;
+    calculateTip();
+}
+
+function updateEmoji(tip) {
+    if (tip <= 10) {
+        document.getElementById('tipEmoji').innerHTML = '&#128542;';
+    } else if (tip > 10 && tip <= 20) {
+        document.getElementById('tipEmoji').innerHTML = '&#128528;';
+    } else {
+        document.getElementById('tipEmoji').innerHTML = '&#128578;';
+    }
 }
 
 function printDetails() {
     let printContent = document.querySelector('.container').outerHTML;
     let printWindow = window.open('', '_blank');
-
-    printWindow.document.write('<html><head><title>Print</title>');
-    printWindow.document.write('<link rel="stylesheet" href="styles.css" type="text/css" />');
-    printWindow.document.write('</head><body>');
+    printWindow.document.write('<html><head><title>Print</title></head><body>');
     printWindow.document.write(printContent);
     printWindow.document.write('</body></html>');
     printWindow.document.close();
     printWindow.print();
-
-    printWindow.onafterprint = function() {
-        printWindow.close();
-        window.focus();
-    };
-
-    resetCalculator();
 }
